262a561fbb3fb96b9d0301532bf12199dda2dd99
Yukari <fla.yu@outlook.com>
2022-01-03 20:35:18 -0300

    Criação do banco.

---
+++ Data/TesteDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class TesteDbContext : DbContext
+    {
+        public TesteDbContext() : base("Name=TesteDb")
+        {
+            Database.SetInitializer<TesteDbContext>(
+                new CreateDatabaseIfNotExist<TesteDbContext>()
+                );
+        }
+    }
+
+}

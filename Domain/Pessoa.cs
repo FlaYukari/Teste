56d0e420d73eccd981cc16dbe4470e647945dbd8
Yukari <fla.yu@outlook.com>
2022-01-03 20:19:38 -0300

    Deixando a arquitetura em DDD

---
+++ Domain/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public int CEP { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Pais { get; set; }
+        public int Telefone { get; set; }
+        public int Celular { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Boolean Sexo { get; set; }
+    }
+}
+
---
+++ Presentation/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult UpDate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UpDate()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
+}

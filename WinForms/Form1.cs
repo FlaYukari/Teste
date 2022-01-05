68b4e609abe748a18640669332fb1fa36858b36d
Yukari <fla.yu@outlook.com>
2022-01-05 17:25:18 -0300

    Instalei o EntityFramework no projeto WinForms.

---
+++ WinForms/Form1.cs
@@ -0,0 +1,31 @@
+﻿using Data;
+using Domain;
+using System;
+using System.Windows.Forms;
+
+namespace WinForms
+{
+    public partial class Form1 : Form
+    {
+        private PessoaDBContext context;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            context = new PessoaDBContext();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pessoa pessoa = new Pessoa()
+            {
+                Nome = "Pessoa teste DevMedia",
+                Endereco = "Rua Augusto Jesus, 103"
+            };
+            context.Pessoas.Add(pessoa);
+
+            context.SaveChanges();
+        }
+    }
+}

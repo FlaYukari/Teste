fe3827dbfb37877d791270da8f3aca697b163a76
Yukari <fla.yu@outlook.com>
2022-01-05 16:20:22 -0300

    Instalei o EntityFramework no projeto Application

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,8 @@ namespace Application
     {
         static void Main(string[] args)
         {
-            Pes
+            PessoaDBContext context = new PessoaDBContext();
+
         }
     }
 }
